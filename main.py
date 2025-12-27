4085ff46109a4e59ee837a0f20c7989c53c2cff3
Gabe Dorval <gabed589@gmail.com>
2025-12-27 11:59:39 -0500

    used redirect and url_for() to redirect to dynamic urls

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask     
+from flask import Flask, redirect, url_for     
 app = Flask(__name__)   # Flask constructor 
   
 # A decorator used to tell the application 
@@ -16,6 +16,21 @@ def hello_name(name):
 @app.route('/blog/<int:postID>')
 def show_blog(postID): 
     return 'Blog Number %d' % postID
+
+@app.route('/admin')  # decorator for route(argument) function
+def hello_admin():  # binding to hello_admin call
+    return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):  # binding to hello_guest call
+    return 'Hello %s as Guest' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':  # dynamic binding of URL to function
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
 
 if __name__=='__main__': 
    app.run(debug=True, host="0.0.0.0")
